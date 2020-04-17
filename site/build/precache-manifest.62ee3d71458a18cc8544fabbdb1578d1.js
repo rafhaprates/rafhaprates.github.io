a63b784dd55330a9cb5ba187a6ee6746c9a3b121
Rafhael Prates <31408774+rafhaprates@users.noreply.github.com>
2020-04-17 14:28:03 -0300

    add9

---
+++ site/build/precache-manifest.62ee3d71458a18cc8544fabbdb1578d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f34a5814e0160ee7ff5914496ff240b",
+    "url": "/site/index.html"
+  },
+  {
+    "revision": "e19353cd89192c7c574f",
+    "url": "/site/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "437629778b21d7628602",
+    "url": "/site/static/js/2.88d80807.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/site/static/js/2.88d80807.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e19353cd89192c7c574f",
+    "url": "/site/static/js/main.bd2c5b96.chunk.js"
+  },
+  {
+    "revision": "40f1c76e56989dda9ea0",
+    "url": "/site/static/js/runtime-main.75ea76d4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/site/static/media/logo.5d5d9eef.svg"
+  }
+]);
